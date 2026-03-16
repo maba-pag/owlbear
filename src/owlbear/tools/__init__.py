604b2df26699a5726e7600bb353d7ef83c02ce50
maba-pag <238680231+maba-pag@users.noreply.github.com>
2026-03-16 21:08:28 +0100

    feat: add re-exports to tools/__init__.py (#814, builder)

--- src/owlbear/tools/__init__.py
+++ src/owlbear/tools/__init__.py
@@ -1,3 +1,26 @@
 """Tool abstractions and registry."""
 
 from __future__ import annotations
+
+from owlbear.tools.ask_user import AskUserToolset
+from owlbear.tools.filesystem import FileToolset
+from owlbear.tools.git_local import GitLocalToolset
+from owlbear.tools.github_api import GitHubToolset
+from owlbear.tools.hooked import HookedToolset
+from owlbear.tools.kanban import KanbanToolset
+from owlbear.tools.mcp_registry import MCPServerRegistry
+from owlbear.tools.protocols import find_toolset, unwrap
+from owlbear.tools.terminal import TerminalToolset
+
+__all__ = (
+    "AskUserToolset",
+    "FileToolset",
+    "GitHubToolset",
+    "GitLocalToolset",
+    "HookedToolset",
+    "KanbanToolset",
+    "MCPServerRegistry",
+    "TerminalToolset",
+    "find_toolset",
+    "unwrap",
+)
