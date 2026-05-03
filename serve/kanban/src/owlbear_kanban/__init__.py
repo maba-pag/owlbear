7c6c010de5eb469a4a42efc5304bd46e1ed5be10
maba-pag <238680231+maba-pag@users.noreply.github.com>
2026-05-04 00:36:07 +0200

    feat: export kanban root API symbols (#1213, builder)

--- serve/kanban/src/owlbear_kanban/__init__.py
+++ serve/kanban/src/owlbear_kanban/__init__.py
@@ -7,14 +7,25 @@ dispatch selector (pick_dispatchable).
 from __future__ import annotations
 
 from owlbear_kanban.dispatch import pick_dispatchable
-from owlbear_kanban.engine import KanbanEngine, WorkSession
+from owlbear_kanban.engine import AgentView, KanbanEngine, WorkSession
+from owlbear_kanban.errors import (
+    ConcurrencyError,
+    CorruptionError,
+    NotFoundError,
+    ValidationError,
+)
 from owlbear_kanban.models import BoardConfig, Task, TaskSummary
 
 __all__ = [
+    "AgentView",
     "BoardConfig",
+    "ConcurrencyError",
+    "CorruptionError",
     "KanbanEngine",
+    "NotFoundError",
     "Task",
     "TaskSummary",
+    "ValidationError",
     "WorkSession",
     "pick_dispatchable",
 ]
