a615918d812d695593d9f3e991e9f09950250d2f
maba-pag <238680231+maba-pag@users.noreply.github.com>
2026-05-12 13:16:37 +0200

    feat: replace PDS monkeypatch with cdn property trap (#1496, builder)

--- serve/cockpit/web/src/main.tsx
+++ serve/cockpit/web/src/main.tsx
@@ -4,41 +4,38 @@ import { load } from '@porsche-design-system/components-js'
 import './tokens.css'
 import App from './App'
 
-const PDS_CDN_SCRIPT = /^https:\/\/cdn\.ui\.porsche\.(com|cn)\/porsche-design-system\/components\/(.+)$/
 const REQUIRED_PDS_ELEMENTS = ['p-button', 'p-icon', 'p-tabs', 'p-tabs-item'] as const
 
-function installPdsRuntimeScriptRewrite(): void {
-  const elementProto = Element.prototype
-  const originalAppendChild = elementProto.appendChild
+function installPdsCdnTrap(): void {
+  const docWithPds = document as { porscheDesignSystem?: Record<string, unknown> }
+  const pds = (docWithPds.porscheDesignSystem ??= {})
+  let assignedCdn: unknown
 
-  elementProto.appendChild = function appendChildWithPdsRewrite<T extends Node>(node: T): T {
-    if (node instanceof HTMLScriptElement && node.src) {
-      const match = node.src.match(PDS_CDN_SCRIPT)
-      if (match) {
-        const docWithPds = document as { porscheDesignSystem?: { cdn?: { url?: string } } }
-        const pds = (docWithPds.porscheDesignSystem ??= { cdn: { url: window.location.origin } })
-        pds.cdn = { ...(pds.cdn ?? {}), url: window.location.origin }
-        node.src = `${window.location.origin}/porsche-design-system/components/${match[2]}`
-      }
-    }
+  Object.defineProperty(pds, 'cdn', {
+    configurable: true,
+    enumerable: true,
+    get(): { url: string } & Record<string, unknown> {
+      const fromAssigned =
+        assignedCdn && typeof assignedCdn === 'object'
+          ? (assignedCdn as Record<string, unknown>)
+          : {}
 
-    return originalAppendChild.call(this, node) as T
-  }
+      return { ...fromAssigned, url: window.location.origin }
+    },
+    set(value: unknown): void {
+      assignedCdn = value
+    },
+  })
 }
-
-installPdsRuntimeScriptRewrite()
 
 async function waitForRequiredPdsElements(): Promise<void> {
   await Promise.all(REQUIRED_PDS_ELEMENTS.map((tagName) => customElements.whenDefined(tagName)))
 }
 
 async function bootstrap(): Promise<void> {
+  installPdsCdnTrap()
   load()
   await waitForRequiredPdsElements()
-    ;(document as { porscheDesignSystem?: { cdn?: { url?: string } } }).porscheDesignSystem = {
-      ...((document as { porscheDesignSystem?: { cdn?: { url?: string } } }).porscheDesignSystem ?? {}),
-      cdn: { url: window.location.origin },
-    }
 
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
